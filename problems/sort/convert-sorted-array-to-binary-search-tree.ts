4c1e8fbb96275d5b7f86cefcc97a6e5cf3337581
Sooji Kim <yh0gogo@gmail.com>
2022-08-24 16:16:22 +0900

    Update solved problem's detailed description

--- problems/sort/convert-sorted-array-to-binary-search-tree.ts
+++ problems/sort/convert-sorted-array-to-binary-search-tree.ts
@@ -1,3 +1,8 @@
+/**
+ * Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+ * A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+ */
+
 class TreeNode {
   val?: number;
   left?: TreeNode | null;
